35fcb20e7a2a095b81321d3c16dea5fb17c3017c
Usuario <Usuario@192.168.0.154>
2016-06-10 23:24:04 +0200

    Finalisado

---
+++ src/boletin25/pkg2/Boletin252.java
@@ -0,0 +1,15 @@
+package boletin25.pkg2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin252 {
+
+    
+    public static void main(String[] args) {
+        Frame window= new Frame();
+        window.setVisible(true);
+    }
+    
+}
